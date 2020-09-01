487db5f1c7923c154eb8d9aab0db2e6056afaea6
Robert Bloxham <acidwash52@gmail.com>
2020-09-01 12:35:46 -0700

    updating styling to chatroom and driver/team show

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,2 +1,11 @@
+const quack = new Audio("/audio/apatheticquack.mp3");
+const enterAudio = new Audio("/audio/enter.wav");
+const exitAudio = new Audio("/audio/exit.wav");
+const messageAudio = new Audio("/audio/message.wav");
+
+function makeItQuack() {
+  quack.play();
+}
+
 
 
