cfc72e88ace176b87c6d8f55b56b6365622244e8
Robert Bloxham <acidwash52@gmail.com>
2020-09-02 11:44:16 -0700

    editing styling

--- public/javascripts/landingpage.js
+++ public/javascripts/landingpage.js
@@ -7,7 +7,7 @@ startBtn.addEventListener('click', playSound)
 
 function playSound(e){
     tones.play(),
-    document.getElementById("lights").style.display="",
+    document.getElementById("lights").style.display="block",
     setTimeout(function () {
         window.location = "/messages";
     }, 7000)
