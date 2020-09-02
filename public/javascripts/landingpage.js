907cc7ab75e4c668dac975bc11766f52d21c3e22
Robert Bloxham <acidwash52@gmail.com>
2020-09-02 11:23:23 -0700

    udpating landing page with sound play functionality

---
+++ public/javascripts/landingpage.js
@@ -0,0 +1,18 @@
+const tones = new Audio('../audio/starttones.wav')
+
+const startBtn = document.getElementById('startBtn')
+
+startBtn.addEventListener('click', playSound)
+
+
+function playSound(e){
+    tones.play(),
+    document.getElementById("lights").style.display="block",
+    setTimeout(function () {
+        window.location = "/messages";
+    }, 7000)
+}
+
+
+
+
