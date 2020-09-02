81df39edceb5c60589a7d9f84ba3d20a67d974e4
Robert Bloxham <acidwash52@gmail.com>
2020-09-01 20:12:56 -0700

    updating profile form

--- models/user.js
+++ models/user.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema
 
+const favoriteTrack = new mongoose.Schema({
+  favoriteTrack: String,
+  aboutFavoriteTrack: String
+})
 
 const userSchema = new mongoose.Schema({
   name: String,
+  alias: String,
   email: String,
   avatar: String,
   googleId: String,
