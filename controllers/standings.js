3986c8dc7cdac151f1df354b6b124f7ba06d62ac
Robert Bloxham <acidwash52@gmail.com>
2020-09-02 20:18:38 -0700

    adding standings view and controller and passing api data to view page

--- controllers/standings.js
+++ controllers/standings.js
@@ -5,13 +5,22 @@ module.exports = {
 }
 
 function index(req, res){
+    let one = 'http://ergast.com/api/f1/current/driverStandings.json'
+    let two = 'http://ergast.com/api/f1/current/constructorStandings.json'
+
+    const requestOne = axios.get(one)
+    const requestTwo = axios.get(two)
     axios
-        .get('http://ergast.com/api/f1/current/driverStandings.json')
-        .then((response) => {
-            console.log(response)
-        })
-        res.render("standings/index", {
-            user: req.user
-        })
+        .all([requestOne, requestTwo])
+        .then(axios.spread((...responses) => {
+            const drivers = responses[0]
+            const constructors = responses[1]
+            res.render("standings/index", {
+            user: req.user,
+            drivers: responses[0].data.MRData.StandingsTable.StandingsLists[0].DriverStandings,
+            constructors: responses[1].data.MRData.StandingsTable.StandingsLists[0].ConsturctorStandings
+            })
+        }))
         .catch(err => console.log(err))
 }
+        
